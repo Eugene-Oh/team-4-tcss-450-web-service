75f234bfefa923094ebb1a16b37a53fc7e3b41d6
AndreLarson <dre.larson7@gmail.com>
2022-05-05 02:34:50 -0700

    add dco

--- routes/verify.js
+++ routes/verify.js
@@ -9,6 +9,26 @@ const pool = require('../utilities').pool
 
 const router = express.Router()
 
+/**
+ * @api {get} /verify/:id Verify a member using their member id
+ * @apiName GetVerify
+ * @apiGroup Verify
+ * 
+ * @apiHeader {String} authorization "username:password" uses Basic Auth 
+ * 
+ * @apiSuccess {String} message "success"
+ * @apiSuccess {boolean} success true when the verification column is updated
+ * 
+ *  * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "success"
+ *       "success": true
+ *     }
+ * 
+ * @apiError (400: Incorrect member id) {String} message "FAILED"
+ * 
+ */ 
 router.get('/:id', async (request, response) => {
     const { id } = request.params
     const theQuery = `UPDATE members SET verification=1 WHERE memberid=$1`
@@ -27,14 +47,13 @@ router.get('/:id', async (request, response) => {
         })
 })
 
-router.post('/email', (request, response) => {
-    const email = request.body.email
-    let link = "https://team-4-tcss-450-web-service.herokuapp.com/verify/" + request.body.memberid
-    sendEmail("team4tcss450@gmail.com", email,"Welcome to our App!", "Please verify your Email account.\n" + link)
-    response.status(200).send({
-        message: "success"
-    })
-})
-
+// router.post('/email', (request, response) => {
+//     const email = request.body.email
+//     let link = "https://team-4-tcss-450-web-service.herokuapp.com/verify/" + request.body.memberid
+//     sendEmail("team4tcss450@gmail.com", email,"Welcome to our App!", "Please verify your Email account.\n" + link)
+//     response.status(200).send({
+//         message: "success"
+//     })
+// })
 
 module.exports = router
