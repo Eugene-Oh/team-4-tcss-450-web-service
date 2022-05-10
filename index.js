c7c13bbab86f3587b33b4156c253bcfd54e262a2
Eugene Oh <eugeneoh911@Gmail.com>
2022-05-09 22:10:37 -0700

    Messaging

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(middleware.jsonErrorInBody)
 
 app.use('/auth', require('./routes/signin.js'))
 
-// app.use('/auth', require('./routes/register.js'))
+app.use('/auth', require('./routes/register.js'))
 
 app.use('/auth', middleware.checkToken, require('./routes/pushyregister.js'))
 
@@ -30,7 +30,6 @@ app.use('/verify', require('./routes/verify.js'))
 app.use('/messages', middleware.checkToken, require('./routes/messages.js'))
 
 app.use('/chats', middleware.checkToken, require('./routes/chats.js'))
-
 
 
 
