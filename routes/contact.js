8a94344d1e792418abd63adb20d9f592692f2461
Nhat Trang <nhat499@uw.edu>
2022-05-12 18:43:34 -0700

    add contanct files

---
+++ routes/contact.js
@@ -0,0 +1,6 @@
+// express is the framework we're going to use to handle requests
+const express = require('express')
+
+//Access the connection to Heroku Database
+const pool = require('../utilities').pool
+
